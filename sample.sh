db22666b6155b33c13b2c26c861a77d86203b1f4
nivethar-cloudside <87644015+nivethar-cloudside@users.noreply.github.com>
2021-07-27 00:26:45 +0530

    Create sample.sh

---
+++ sample.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello $LOGNAME !"
+echo "Welcome to the world of Linux automation"
+echo "The host name is $HOSTNAME"
+echo "Your present working directory is `pwd`"
+echo "Thank you, see you soon $LOGNAME."
